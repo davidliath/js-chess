34b3b7c7015d2b6650d4b24e0ac831667798c21a
David Rosas <davidrosasv43@gmail.com>
2024-08-25 03:51:07 +0000

    board and beiges

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const gameBoard = document.querySelector("#gameboard")
 const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 
-const StartPieces = [
-    
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+function createBoard() {
+    startPieces.forEach((startPiece) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.classList.add('beige')
+
+        gameBoard.append(square)
+    })
+}
+createBoard()
